b1b9feb4b671c32e0f8cc71aefe3bfd7b1fe8133
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2024-03-19 19:41:12 -0300

    Corrige os testes

--- exercicios/aula_4_andre/storage.py
+++ exercicios/aula_4_andre/storage.py
@@ -1,48 +1,20 @@
 import json
-import os
 
-dados_amostra = {
-    "name": "John Doe",
-    "age": 30,
-    "city": "Example City",
-    "isStudent": False,
-    "grades": [85, 90, 78],
-    "address": {
-    "street": "123 Main Street",
-    "zipCode": "12345",
-    "country": "Exampleland"
-    }
-}
 
 def converter_dicionario_de_dados_para_string_json(dados):
-    return json.dumps(dados)
+    return json.dumps(dados, indent=2)
+
 
 def converter_string_json_para_dicionario(dados):
     return json.loads(dados)
 
+
 def armazenar_dicionario_de_dados_como_arquivo_json(dados, nome_arquivo):
-    with open(nome_arquivo, 'w') as file:
+    with open(nome_arquivo, "w") as file:
         json.dump(dados, file, indent=2)
-   
-    # with open(nome_arquivo, 'r') as file:
-    #     json_carregado = json.load(file)
-    #     if json_carregado == dados:
-    #         return "Success"
-    #     else:
-    #         return "No success"
+
 
 def ler_arquivo_json_como_dicionario(nome_arquivo):
-    with open(nome_arquivo, 'r') as file:
-        return json.load(file)      
+    with open(nome_arquivo, "r") as file:
+        return json.load(file)
 
-# if __name__ == "__main__":
-#     dict_para_str_json = converter_dicionario_de_dados_para_string_json(dados_amostra)
-#     str_json_para_dict = converter_string_json_para_dicionario(dict_para_str_json)
-#     armazenar_dicionario_de_dados_como_arquivo_json(str_json_para_dict, "arquivo.json")
-#     json_para_dict = ler_arquivo_json_como_dicionario("arquivo.json")
-#     print(json_para_dict)
-
-#     file_path = "arquivo.json"
-#     if os.path.exists(file_path):
-#         os.remove(file_path)
-    
--- exercicios/aula_4_andre/test_storage.py
+++ exercicios/aula_4_andre/test_storage.py
@@ -1,90 +1,116 @@
-import json
+from io import StringIO
+from textwrap import dedent
+from unittest.mock import mock_open, patch
+
 import pytest
-from  unittest.mock import mock_open, patch
 
 from .storage import (
+    armazenar_dicionario_de_dados_como_arquivo_json,
     converter_dicionario_de_dados_para_string_json,
     converter_string_json_para_dicionario,
-    armazenar_dicionario_de_dados_como_arquivo_json,
-    ler_arquivo_json_como_dicionario
+    ler_arquivo_json_como_dicionario,
 )
+
+
+@pytest.fixture
+def buffer():
+    return StringIO()
+
 
 @pytest.fixture
 def dict_dados_amostra():
-  yield {
-      "name": "John Doe",
-      "age": 30,
-      "city": "Example City",
-      "isStudent": False,
-      "grades": [85, 90, 78],
-      "address": {
-      "street": "123 Main Street",
-      "zipCode": "12345",
-      "country": "Exampleland"
-      }
-  }
-  
+    yield {
+        "name": "John Doe",
+        "age": 30,
+        "city": "Example City",
+        "isStudent": False,
+        "grades": [85, 90, 78],
+        "address": {
+            "street": "123 Main Street",
+            "zipCode": "12345",
+            "country": "Exampleland",
+        },
+    }
+
 
 @pytest.fixture
 def json_dados_amostra():
-  return '''{
-  "name": "John Doe",
-  "age": 30,
-  "city": "Example City",
-  "isStudent": false,
-  "grades": [
-    85,
-    90,
-    78
-  ],
-  "address": {
-    "street": "123 Main Street",
-    "zipCode": "12345",
-    "country": "Exampleland"
-  }
-}'''
-  
-  # teste = json.dumps(
-  #   {
-  #     "name": "John Doe",
-  #     "age": 30,
-  #     "city": "Example City",
-  #     "isStudent": False,
-  #     "grades": [85, 90, 78],
-  #     "address": {
-  #       "street": "123 Main Street",
-  #       "zipCode": "12345",
-  #       "country": "Exampleland"
-  #     }
-  #   },
-  #   indent = 2)
-  # print(teste)
-  
+    return dedent(
+        """\
+        {
+          "name": "John Doe",
+          "age": 30,
+          "city": "Example City",
+          "isStudent": false,
+          "grades": [
+            85,
+            90,
+            78
+          ],
+          "address": {
+            "street": "123 Main Street",
+            "zipCode": "12345",
+            "country": "Exampleland"
+          }
+        }"""
+    )
 
-def test_converter_dicionario_de_dados_para_string_json(dict_dados_amostra, json_dados_amostra):
-  # assim fica no estilo triple a: arrange, act, assert
-  resultado = converter_dicionario_de_dados_para_string_json(dict_dados_amostra)
-                                                             
-  assert resultado  == json_dados_amostra
+
+def test_converter_dicionario_de_dados_para_string_json(
+    dict_dados_amostra, json_dados_amostra
+):
+    # assim fica no estilo triple a: arrange, act, assert
+    resultado = converter_dicionario_de_dados_para_string_json(dict_dados_amostra)
+
+    assert isinstance(resultado, str)
+    assert resultado == json_dados_amostra
+
 
 def test_converter_string_json_para_dicionario(dict_dados_amostra, json_dados_amostra):
-  
-  resultado = converter_string_json_para_dicionario(json_dados_amostra)
 
-  assert resultado == dict_dados_amostra
+    resultado = converter_string_json_para_dicionario(json_dados_amostra)
 
-def test_armazenar_dicionario_de_dados_como_arquivo_json():
-  
-  with patch('builtins.open', mock_open()) as mock_file:
-    filename = 'test.json'
-    data = {'key': 'value'}
+    assert isinstance(resultado, dict)
+    assert resultado == dict_dados_amostra
 
-    armazenar_dicionario_de_dados_como_arquivo_json(data, filename)
-    
-    mock_file.assert_called_once_with(filename, 'w')
-    mock_file().write.assert_called_once_with(json.dumps(data, indent=2))
-  
 
-def test_ler_arquivo_json_como_dicionario():
-  # assert ler_arquivo_json_como_dicionario("teste.json") == dict_dados_amostra    
-  pass
+def test_armazenar_dicionario_de_dados_como_arquivo_json(
+    tmp_path, dict_dados_amostra, json_dados_amostra
+):
+    # sobre a fixture tmp_path: https://docs.pytest.org/en/6.2.x/tmpdir.html
+
+    conteudo_esperado = json_dados_amostra
+    filename = tmp_path / "test.json"
+
+    armazenar_dicionario_de_dados_como_arquivo_json(dict_dados_amostra, filename)
+
+    with open(filename, "r") as file:
+        conteudo_que_foi_escrito = file.read()
+
+    assert conteudo_que_foi_escrito == conteudo_esperado
+
+
+def test_armazenar_dicionario_de_dados_como_arquivo_json_de_outra_forma():
+    m = mock_open()
+    with patch("builtins.open", m):
+        filename = "test.json"
+        data = {"key": "value"}
+
+        armazenar_dicionario_de_dados_como_arquivo_json(data, filename)
+
+    handle = m()
+
+    # TODO: precisamos fazer a asserção das chamadas
+    print(handle.write.mock_calls)
+
+
+def test_ler_arquivo_json_como_dicionario(
+    tmp_path, dict_dados_amostra, json_dados_amostra
+):
+    with open(tmp_path / "asdf.json", "w") as file:
+        file.write(json_dados_amostra)
+
+    resultado = ler_arquivo_json_como_dicionario(tmp_path / "asdf.json")
+
+    assert isinstance(resultado, dict)
+    assert resultado == dict_dados_amostra
