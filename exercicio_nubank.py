be72decaf94f6b3ce6c9cb10668faf6327efabaf
Rodrigo Eiti Kimura <rodrigoeitikimura@gmail.com>
2024-04-30 22:10:45 -0300

    Adiciona enunciado do exercício

---
+++ exercicio_nubank.py
@@ -0,0 +1,46 @@
+# PROPOSTA: analisar os gastos do nubank
+
+
+# parte 0 - setup
+# dar nome ao projeto (será o nome do repositório tbm)
+# criar:
+# - pasta
+# - repo local
+# - repo remoto (github)
+# - ambiente virtual
+
+
+# parte 1 - autenticação e camada de cache
+#
+# rodar o cli para criar certificado que será usado para autenticação
+# adicionar esse arquivo no .gitignore
+# criar função que armazena os dados das transações em um arquivo
+
+
+# parte 2 - sumarização
+#
+# sumarizar por categoria
+#
+# - soma
+# - contagem
+# - média
+#
+# criar função genérica que faça isso, recebendo a "estratégia" de sumarização como parâmetro
+#
+# sumarizar por ano
+# - soma
+# - contagem
+# - média
+#
+# criar função genérica que faça isso, recebendo a "estratégia" de sumarização como parâmetro
+
+
+# parte 3 - visualização
+#
+# criar gráfico de barras a partir do resultado da sumarização
+# sugestão: usando matplotlib ou seaborn
+
+
+# parte 4 - parametrização interativa
+#
+# criar função que pergunta qual o tipo de sumarização desejada, gera seus resultados e exibe seu gráfico
